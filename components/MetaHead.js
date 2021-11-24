0519d19f9325efbf2a12b87de43c8a4142c68983
Tansi Jones <tansijones@outlook.com>
2021-11-25 01:22:12 +0200

    Clean up Project

---
+++ components/MetaHead.js
@@ -0,0 +1,12 @@
+import Head from "next/head";
+
+export default function MetaHead() {
+  return (
+    <>
+      <Head>
+        <title>The Weather Today</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </>
+  );
+}
